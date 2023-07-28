bc8189685dca88034fa7b1cb4795c62d7d89bd0b
Leonardo Rodrigues Martins <117056433+Leonardordgm@users.noreply.github.com>
2023-07-28 14:33:21 -0300

    Add files via upload

---
+++ arrays/exercicios/nPares.js
@@ -0,0 +1,8 @@
+const numeros = [1,2,3,4,5,6,7,16,8,9];
+
+for (let i = 0; i < numeros.length; i++) {
+    const numero = numeros[i];
+    if (numero % 2 === 0) {
+        console.log(numero)
+    };
+}
